f6279dd0b44d1816fc837ef59cf8aa0151ab90ef
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-04-22 17:21:39 +0000

    change script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022 Joanne Santhosh All rights reserved
+//
+// Created by: Joanne Santhosh
+// Created on: Apr 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+
