3726b4f6f7c8a60cb8a713b967f98a2a74b0ec8f
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-04-25 17:11:55 +0000

    change 2 files

--- js/script.js
+++ js/script.js
@@ -4,5 +4,29 @@
 // Created on: Apr 2022
 // This file contains the JS functions for index.html
 
-'use strict'
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register(
+    "/ICS2O-Unit5-01-HTML/sw.js",
+    {
+      scope: "/ICS2O-Unit5-01-HTML-Slider-Example/",
+    }
+  )
+}
 
+/**
+ * This function updates the slider value.
+ */
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
+}
+
+/**
+ * This function displays the slider value.
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML =
+    "<p>Value is: " + slider.value + "</p>"
+}
